d1ad0807959984714dcb7eb7ae4ff9b9917c94c6
fmamah777 <fm.mamah@gmail.com>
2022-01-25 20:13:08 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - printing lower case letters followed with a new line.
+*
+* Return: (0)
+*/
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+if (x != 'e' && x != 'q')
+putchar(x);
+putchar('\n');
+return (0);
+}
