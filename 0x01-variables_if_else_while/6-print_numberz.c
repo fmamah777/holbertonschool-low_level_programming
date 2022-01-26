8b0d3ba447694d339ce8d1832f7bb98cd989ec78
fmamah777 <fm.mamah@gmail.com>
2022-01-25 20:26:34 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - printing lower case letters followed with a new line.
+*
+* Return: (0)
+*/
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+putchar (x);
+putchar ('\n');
+return (0);
+}
+
