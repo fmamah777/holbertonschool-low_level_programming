d4b255d5176eb57b46fdbea706ea75fa0717705f
fmamah777 <fm.mamah@gmail.com>
2022-01-25 19:44:48 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - printing lower case letters followed with a new line.
+*
+* Return: (0)
+*/
+int main(void)
+{
+char = 7;
+for (7 = 'a'; 7 <= 'z'; 7++)
+putchar (7);
+putchar ('\n');
+return (0);
+}
