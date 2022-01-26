773012ca83d35cf465b97cd6ae8483c5190bb5ee
fmamah777 <fm.mamah@gmail.com>
2022-01-25 21:27:08 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - printing lower case letters followed with a new line.
+*
+* Return: (0)
+*/
+int main(void)
+{
+int x;
+for (x = '0'; x < 10; x++)
+{
+putchar (x % 10 + '0');
+if (x != 9)
+{
+putchar (44);
+putchar (' ');
+}
+}
+putchar ('\n');
+return (0);
+}
