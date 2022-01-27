18c11e333f3ae86cbf308d4d416fea551d9cba4b
fmamah777 <fm.mamah@gmail.com>
2022-01-26 22:02:59 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int);
+{
+n = (n % 10);
+if (n >= 0)
+{
+_putchar('0' + n);
+return (n);
+}
+else if (n < 0)
+{
+n = (n * -1);
+_putchar('0' + n);
+return (n);
+}
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
 int print_sign(int n);
+int print_last_digit(int);
 /* _putchar prototype */
 int _putchar(char c);
 
