049ab9eb63a962c325179448f37e1a040b0d6fb7
fmamah777 <fm.mamah@gmail.com>
2022-01-27 00:46:07 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table -will multiply by 9
+ *
+ * Return: Always (0)
+ */
+void times_table(void)
+{
+	int length;
+	int height;
+	int product;
+	for (length = 0; length <=9; length++)
+	{
+	putchar('0');
+	for (height =1; height <= 9; height++)
+	{
+	putchar('0');
+	for (height = 1; height <= 9; height++)
+{
+	product = length * height;
+	putchar(',');
+	putchar(' ');
+	if (product <= 9)
+{
+	putchar(' ');
+}
+	else
+{
+	putchar((product / 10) + '0');
+}
+	putchar((product % 10) + '0');
+}
+	putchar('\n');
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,7 @@ int _abs(int);
 int print_sign(int n);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 /* _putchar prototype */
 int _putchar(char c);
 
