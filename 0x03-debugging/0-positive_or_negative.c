864b2ad5129497547911fdda30954ad4e79de590
fmamah777 <fm.mamah@gmail.com>
2022-02-01 13:11:45 -0800

    0-positive_or_negative.c

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,23 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
-* main - main determines if number is pos, 0, or neg.
-*
-* %d: points to the number
-*/
+  * positive_or_negative - determines a number's polarity
+  * Return: 0 (Success)
+  */
 void positive_or_negative(int n)
 {
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
 }
