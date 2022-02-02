9526b13aa4ed3168dea179531f99a72135ea5036
fmamah777 <fm.mamah@gmail.com>
2022-02-02 10:50:04 -0800

    int updates

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,24 @@
-#include "main.h"
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * print_diagonal - this prints a diagonal line in the terminal with spaces.
- * @n: n is a parameter
- * Return: (void)
- */
-
+  * print_diagonal - Entry Point
+  * @n: number of times print '_'
+  * Return: 0 (Success)
+  */
 void print_diagonal(int n)
 {
-	int z;
-	int k;
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
 	for (z = 0; z < n; z++)
 	{
-		for (k = 0; k < z; k++)
-		{
+		for (k = 0; k < i; k++)
 			_putchar(' ');
-		}
 		_putchar('\\');
-
 		_putchar('\n');
 	}
 }
