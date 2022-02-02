88af0c2b6c19b536e721dfa792c375bbfbddaa96
fmamah777 <fm.mamah@gmail.com>
2022-02-02 12:19:12 -0800

    new changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * main - this function will print an integer
+ * print_number - this function will print an integer
  *@n: will be printed as the integer in the terminal
  * Return: 0
  */
 void print_number(int n)
 {
 	unsigned int num, div = 10;
-
 	/*
-	 * if negative number print -x and (n)
+	 * if negative number print -1 and (n)
 	 * then print each number that is there.
 	 * or else print whatever digit appears to be in front and iterate upwards.
 	 */
