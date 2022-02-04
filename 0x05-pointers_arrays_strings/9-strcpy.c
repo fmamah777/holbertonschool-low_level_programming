1447dd49c7fc8643279b2062c903208a6015f0e6
fmamah777 <fm.mamah@gmail.com>
2022-02-03 19:56:00 -0800

    removed white space

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,7 +5,7 @@
  * _strcpy - function copies a string
  * @dest: destination of copied string
  * @src: string to be copied
- * Return: always 0 
+ * Return: always 0
  */
 
 char *_strcpy(char *dest, char *src)
