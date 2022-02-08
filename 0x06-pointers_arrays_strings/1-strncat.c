bcd4072b78c02c26b72a5cb311da59bc2862277f
fmamah777 <fm.mamah@gmail.com>
2022-02-07 18:03:32 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holbeerton.h"
+
+/**
+ * _strncat - A function that concatenates two strings.
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int destlen;
+
+	for (destlen = 0; dest[destlen] != '\0'; destlen++)
+	{}
+	for (i = 0; i < n; i++)
+	{
+		dest[destlen + i] = src[i];
+		if (src[i] == '\0')
+			break;
+	}
+	dest[destlen + i] = '\0';
+	return (dest);
+}
