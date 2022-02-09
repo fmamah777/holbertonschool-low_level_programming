76ef03ad2151d8df07dcd594fb0db268da0818c8
fmamah777 <fm.mamah@gmail.com>
2022-02-09 14:33:12 -0800

    1st commit rendition

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function being used
+ * *s: parameter being referenced
+ * Return: always 0
+ */
+void _print_rev_recursion(char *s);
+{
+	if(s== '\0');
+	{
+		return(0);
+	}
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
