aef3ede1125cf4e232293757e0ae716c0699383a
fmamah777 <fm.mamah@gmail.com>
2022-02-09 22:04:37 -0800

    test

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,42 +1,29 @@
 #include "main.h"
 
 /**
-* is_prime_number - function used to find prime number
-* @n: number being evaluated
-* Return: is number prime?
-*/
+  * is_prime - recursively determines if number is prime
+  * @n: returns if n is prime or not
+  * @i: iterator to track down the primality of n
+  * Return: int, 1 if true, 0 if false
+  */
+int is_prime(int n, int i)
+{
+	if (i == n)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else
+		return (is_prime(n, i + 1));
+}
 
+/**
+  * is_prime_number - determines if number is prime
+  * @n: determine if n is prime
+  * Return: int, 1 if true, 0 if false
+  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
-	{
 		return (0);
-	}
-
-	if (n >= 2 && n <= 3)
-	{
-		return (1);
-	}
-	return (_prime_calc(2, n));
-}
-
-
-/**
-* _prime_calc - function used to calculate prime numbers
-* @x: evaluated number
-* @num: evaluated number
-* Return: is number prime?
-*/
-
-int _prime_calc(int x, int num)
-{
-	if (num % x == 0)
-	{
-		return (0);
-	}
-	if (x == num / 2)
-	{
-		return (1);
-	}
-	return (_prime_calc(x + 1, num));
+	return (is_prime(n, 2));
 }
