12f372a4275cdea875c5b1f1df31d88002ad75e6
fmamah777 <fm.mamah@gmail.com>
2022-02-16 20:56:23 -0800

    double pointer

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * alloc_grid - the function being used
+ *@width: first parameter
+ *@height: the second parameter used
+ *Return: NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid = NULL;
+	int *row = NULL;
+	int x, y;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (y = 0; y < height; y++)
+	{
+		row = malloc(width * sizeof(int));
+		if (row == NULL)
+		{
+			for (y--; y >= 0; y--)
+			{
+				free(grid[y]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (x = 0; x < width; x++)
+		{
+			row[x] = 0;
+		}
+		grid[y] = row;
+	}
+	return (grid);
+}
