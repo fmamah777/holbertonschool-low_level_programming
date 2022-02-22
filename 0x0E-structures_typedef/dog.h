4425f4dac9a57b416c4ad98db2327857814bf87c
fmamah777 <fm.mamah@gmail.com>
2022-02-22 13:14:43 -0800

    snoop dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+/**
+ * struct dog - structure that connects with dog
+ * @name: name of the character
+ * @age: float
+ * @owner: dog owner ?
+ */
+
+typedef struct dog
 {
-	struct dog
-	char * name;
+	struct dog;
+	char *name;
 	float age;
 	char * owner;
 }
+dog_e *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
