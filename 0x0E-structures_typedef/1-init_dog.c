543fe212fa80dfb52a70f16670464fb78e3bd581
fmamah777 <fm.mamah@gmail.com>
2022-02-22 15:29:25 -0800

    variable init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - function being used
+ * @d: pointer that points to dog
+ * @name: parameter that popints to the name
+ * @owner: parameter that points to owner 
+ * @age: float age parameter ?
+ */ 
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
