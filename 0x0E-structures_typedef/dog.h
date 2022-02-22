1415eafd9fafee1727740f47e8258013696e034f
fmamah777 <fm.mamah@gmail.com>
2022-02-22 15:16:19 -0800

    tests

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,15 +8,17 @@
  * @owner: dog owner ?
  */
 
-typedef struct dog_e
+typedef struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-} dog_e;
+	char *owner;
+} dog_t;
 
-dog_e *new_dog(char *name, float 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #endif /* DOG_H */
