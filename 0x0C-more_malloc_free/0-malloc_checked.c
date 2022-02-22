890fcc1d09f42e26b23b99bd441d0c8273d7cca3
fmamah777 <fm.mamah@gmail.com>
2022-02-21 22:55:25 -0800

    fixed push issues

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,30 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	    char *c;
-	        int *i;
-		    float *f;
-		        double *d;
+ * malloc_checked - main function
+ * @b: main integer
+ * Return: 0
+ */
 
-			    c = malloc_checked(sizeof(char) * 1024);
-			        printf("%p\n", (void *)c);
-				    i = malloc_checked(sizeof(int) * 402);
-				        printf("%p\n", (void *)i);
-					    f = malloc_checked(sizeof(float) * 100000000);
-					        printf("%p\n", (void *)f);
-						    d = malloc_checked(INT_MAX);
-						        printf("%p\n", (void *)d);
-							    free(c);
-							        free(i);
-								    free(f);
-								        free(d);
-									    return (0);
+void *malloc_checked(unsigned int b)
+{
+
+	unsigned int *ptr = malloc(b);
+			if (!ptr)
+			{
+				exit(98);
+			}
+			return (ptr);
+}
