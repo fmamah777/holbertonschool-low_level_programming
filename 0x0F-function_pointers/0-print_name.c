9700cf9eec0ca392971458ff7d22154828ffe547
fmamah777 <fm.mamah@gmail.com>
2022-02-23 11:18:38 -0800

    bob dylan

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function being used
+ * @f: pointer being used
+ * @name: name of the person
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
