2517218a08eea62e054863097a388e901073bef2
fmamah777 <fm.mamah@gmail.com>
2022-02-22 20:29:59 -0800

    commit attempt

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,25 @@
 #include "dog.h"
 
 /**
- * new_dog - main function, adds new dog
- * @name: stores name value
- * @age: stores gotcha of age
- * @owner: stores owner name
- * Return: g0t
- */
+* new_dog - entry point.  function to create new dog
+* @name: dog name
+* @age: dog age
+* @owner: owner name
+* Return: name of new dog
+*/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
-		return (NULL);
+	if (new_dog != NULL)
+	{
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
 	return (new_dog);
+	}
+	else
+		return (NULL);
 }
