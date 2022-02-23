f9433849caea59b1fbf586efa1d20f806214d426
fmamah777 <fm.mamah@gmail.com>
2022-02-22 21:32:18 -0800

    final attemot

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,50 @@
 #include "dog.h"
-
 /**
-* new_dog - entry point.  function to create new dog
-* @name: dog name
-* @age: dog age
-* @owner: owner name
-* Return: name of new dog
-*/
+ * new_dog - function name is new dog
+ * @name: char name ?
+ * @age: float age
+ * @owner: char is the owner
+ * Return: Null if fails
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	dog_t *new_dog;
+	char *cpyname, *cpyowner;
+	int namelen, ownerlen, i, x;
 
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog != NULL)
-	{
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
-	}
-	else
+	for (namelen = 0; name[namelen] != '\0'; namelen++)
+		;
+	namelen++;
+	for (ownerlen = 0; owner[ownerlen] != '\0'; ownerlen++)
+		;
+	ownerlen++;
+	cpyname = malloc(sizeof(char) * namelen);
+	if (cpyname == NULL)
 		return (NULL);
+	cpyowner = malloc(sizeof(char) * ownerlen);
+	if (cpyowner == NULL)
+	{
+		free(cpyname);
+		return (NULL);
+	}
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		free(cpyname);
+		free(cpyowner);
+		return (NULL);
+	}
+	for (i = 0; i < namelen; i++)
+	{
+		cpyname[i] = name[i];
+	}
+	for (x = 0; x < ownerlen; x++)
+	{
+		cpyowner[x] = owner[x];
+	}
+	new_dog->name = cpyname;
+	new_dog->age = age;
+	new_dog->owner = cpyowner;
+	return (new_dog);
 }
