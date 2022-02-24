b9763e6bc53e7e63520051f69f0e7ebe45ae86df
fmamah777 <fm.mamah@gmail.com>
2022-02-23 16:42:50 -0800

    array it

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - interates the array
+* @array: array used
+* @size: fuction size of the array ?
+* @action: pointer 
+*Return: Void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			action(array[x]);
+		}
+	}
+}
