f33fc5c940b59f592c7a06519bc9abbcaf642825
fmamah777 <fm.mamah@gmail.com>
2022-02-23 16:00:40 -0800

    fixed header issues

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * int_index - function being used
+ * int_index - function being used to find int
  * @size: the size
  * @array: the array
  * @cmp: pointer to the function to be used to compare values
+ * Return: -1 if theres no matches
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -17,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (x = 0; x < size; x++)
 	{
 		if (cmp(array[x]))
-			return (x);
+		return (x);
 	}
 	return (-1);
 }
