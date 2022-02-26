81f24dbdb83b5209ae049d547aa7d54fd0ef590f
fmamah777 <fm.mamah@gmail.com>
2022-02-25 18:37:57 -0800

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function being used.
+ * @n: first parameter
+ * @ Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list fm;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(fm, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(fm, int);
+	}
+	va_end(fm);
+
+	return (sum);
+}	
