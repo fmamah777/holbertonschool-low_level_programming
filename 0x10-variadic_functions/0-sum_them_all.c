85210a188e4680de8103c6a9cf4edf39fe00c78d
fmamah777 <fm.mamah@gmail.com>
2022-02-25 19:31:19 -0800

    debugging

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,21 +9,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list fm;
+	va_list xy;
 	int sum = 0;
 	unsigned int i;
 
 	if (n == 0)
 		return (0);
 
-	va_start(fm, n);
+	va_start(xy, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(fm, int);
+		sum += va_arg(xy, int);
 	}
 
-	va_end(fm);
+	va_end(xy);
 
 	return (sum);
 }
