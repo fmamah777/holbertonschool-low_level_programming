a72bcb8588655dcac91c389de7fc7cf0dd47a42f
fmamah777 <fm.mamah@gmail.com>
2022-03-09 21:58:54 -0800

    final test

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * pop_listint - funtion being used deletes head node of linked list
- * @head: pointer to list
- * Return: returns head node i
- */
-
+  * pop_listint - remove head of list and return it
+  * @head: head of list
+  * Return: int, data in removed node
+  */
 int pop_listint(listint_t **head)
 {
+	listint_t *del;
+	int data;
 
-	listint_t *temp;
-
-	int x;
-
-	if (head == NULL || *head == NULL)
+	if (!head || !*head)
 		return (0);
-	x = (*head)->x;
-	temp = *head;
+
+	del = *head;
 	*head = (*head)->next;
-	free(temp);
-		return (x);
+	data = del->n;
+	free(del);
+	return (data);
 }
