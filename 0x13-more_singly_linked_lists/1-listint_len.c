13d8eb9827c6aa8a336e6b94c4150655b6fea8d4
fmamah777 <fm.mamah@gmail.com>
2022-03-09 21:55:10 -0800

    fixing more errors

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,13 +3,14 @@
 
 /**
  * listint_len - function being used
-* @h: list
-* Return: elements
-*/
+ * @h: list
+ * Return: elememts
+ */
 
 size_t listint_len(const listint_t *h)
 {
 	size_t s = 0;
+
 	while (h)
 {
 	h = h->next;
