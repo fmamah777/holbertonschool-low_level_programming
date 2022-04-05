da9f133f7a45bca680da7da3e0b08ad44156c192
fmamah777 <fm.mamah@gmail.com>
2022-04-04 21:59:27 -0700

    pushing files

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a dlistint_t list
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+
