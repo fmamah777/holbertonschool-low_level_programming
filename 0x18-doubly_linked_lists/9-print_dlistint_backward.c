dcccd89ff38bb7209492682b53c98a0047238096
fmamah777 <fm.mamah@gmail.com>
2022-04-04 22:20:10 -0700

    fixing unchanged errors

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -3,36 +3,50 @@
 #include <stdlib.h>
 
 /**
-* delete_dnodeint_at_index - is a function that deletes the node at index
-* of a dlistint_t linked list
-* @head: is the double pointer to the head of the list
-* @index: this is the index of the node to delete
-* Return: returns 1 for success, -1 for failure
+* delete_dnodeint_at_index - funtion deletes the node
+* @head: head of double linked list
+* @index: index of the node to delete
+* Return: returns 1 for success, else -1
 */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *current = *head;
-unsigned int i = 0;
-if (*head == NULL || dlistint_len(current) < index + 1)
-return (-1);
-if (!index)
-{
-(*head) = current->next;
-if (current->next)
-current->next->prev = NULL;
-current->next = NULL;
-free(current);
-return (1);
-}
-while (i < index)
-{
-current = current->next;
-i++;
-}
-current->prev->next = current->next;
-if (current->next)
-current->next->prev = current->prev;
-free(current);
-return (1);
+	dlistint_t *temp = *head, *prev = NULL;
+	unsigned int count = 0;
+
+	if (!(head && *head))
+		return (-1);
+	if (index == 0)
+	{
+		temp = *head;
+		if ((*head)->next)
+		{
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+		}
+		else
+			*head = NULL;
+		free(temp);
+		return (1);
+	}
+	while (temp && (count < index))
+	{
+		prev = temp;
+		temp = temp->next;
+		count++;
+	}
+	if (temp == NULL)
+		return (-1);
+	if (prev != NULL)
+	{
+		if (temp->next != NULL)
+		{
+			temp->next->prev = temp->prev;
+		}
+		temp->prev->next = temp->next;
+	}
+	temp->next = NULL;
+	temp->prev = NULL;
+	free(temp);
+	return (1);
 }
