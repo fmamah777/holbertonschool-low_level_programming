9076b677e8800b1c0a54f9bd8c670507a8c305c4
fmamah777 <fm.mamah@gmail.com>
2023-03-30 20:03:23 -0500

    new binary file

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,61 +1,47 @@
 #include "search_algos.h"
 
-
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * print_subarray - print subarray between two indexes
+ * @array: the array to print
+ * @i: start index
+ * @j: end index
  */
-void print_array(const int *array, size_t size)
+void print_subarray(int *array, size_t i, size_t j)
 {
-    size_t i;
+	size_t k;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (k = i; k < j; k++)
+		printf("%d, ", array[k]);
+	printf("%d\n", array[k]);
 }
 
 /**
-* binary_search - Searches for value in sorted array
-* @array: Pointer to 1st element
-* @size: Number of elements
-* @value: Value to search for
-* Return: Index of value or -1 if NULL
-**/
-
+ * binary_search - searches for a value in a sorted array of ints using binary
+ * @array: array to be searched
+ * @size: size of array
+ * @value: key to search for
+ * Return: index of value of -1
+ */
 int binary_search(int *array, size_t size, int value)
 {
-    int low = 0;
-    int high = size;
+	size_t i = 0;
+	size_t j = size - 1;
+	size_t mid;
 
-    if (array == NULL)
-            return (-1);
-    
-    while (low < high)
-    {
-        int key = (high - low) / 2 + low;
-        
-        printf("Searching in array: ");
-        print_array(array, high);
-        
-        if (array[key] == value)
-            return key;
-        
-        if (array[key] > value)
-        {
-            high = key - 1;
-        }
-        else 
-        {
-            low = key + 1;
-        }
-    }
-    return (-1);
+	if (!array)
+		return (-1);
+	while (i <= j)
+	{
+		print_subarray(array, i, j);
+		mid = (i + j) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			j = mid - 1;
+		else if (array[mid] < value)
+			i = mid + 1;
+	}
+
+	return (-1);
 }
